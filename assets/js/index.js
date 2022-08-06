201b9c5c655a4f2beb0e263e65969158bea70119
labor-day <shaffer.scott@yahoo.com>
2022-08-06 13:39:16 -0600

    working on reset button

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,74 @@ let board = new Board(); // creates a new game board
 console.log(board.grid);
 
 // Your code here
+
+window.addEventListener("DOMContentLoaded", event => {
+
+  let hiddenGameOver = document.createElement("h2");
+  hiddenGameOver.innerText = "YOU WIN!";
+  hiddenGameOver.classList.add("hide");
+  document.body.append(hiddenGameOver);
+
+  let resetButton = document.createElement("button");
+  resetButton.innerText = "Reset Game";
+  document.body.append(resetButton);
+
+  resetButton.addEventListener("click", event => {
+    board = new Board();
+    hiddenGameOver.classList.add("hide");
+    hiddenGameOver.classList.remove("show");
+
+    //reset all squares???
+    //change bg to white/transparent?
+    ///change innertext to nothing?
+  })
+
+  let gameGrid = document.createElement("div");
+  gameGrid.id = "board";
+  document.body.append(gameGrid);
+
+  board.grid.forEach(
+    (row, rowNumber) => {
+      row.forEach(
+        (element, colNumber) => {
+
+          //create a square for each value of the array
+          let container = document.createElement("div");
+          container.className = "square";
+          //container.innerText = `${rowNumber}, ${colNumber}`;
+
+          //store its row and column as data
+          container.setAttribute("data-row", rowNumber);
+          container.setAttribute("data-col", colNumber);
+
+          gameGrid.append(container);
+        }
+      );
+    }
+  );
+
+  const gameGridClickListener = (event) =>  {
+
+    let row = event.target.dataset.row;
+    let col = event.target.dataset.col;
+    let result = board.makeHit(row, col)
+
+    if (result) {
+      event.target.classList.add("correct");
+      event.target.innerText = result;
+    }
+    if (!result) {
+      event.target.classList.add("incorrect");
+    }
+
+    //if game over, display win and remove event listeners
+    if (board.isGameOver()) {
+      hiddenGameOver.classList.add("show");
+      hiddenGameOver.classList.remove("hide");
+
+      gameGrid.removeEventListener("click", gameGridClickListener);
+    }
+  }
+  gameGrid.addEventListener("click", gameGridClickListener);
+
+});
