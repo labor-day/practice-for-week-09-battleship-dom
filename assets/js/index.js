0766beaefdaeb823dacf95df2d0c59c4afff0e08
labor-day <shaffer.scott@yahoo.com>
2022-08-06 16:16:46 -0600

    completed basics

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,39 +19,46 @@ window.addEventListener("DOMContentLoaded", event => {
   resetButton.innerText = "Reset Game";
   document.body.append(resetButton);
 
+  let gameGrid;
+
+  let addGrid = () => {
+    gameGrid = document.createElement("div");
+    gameGrid.id = "board";
+    document.body.append(gameGrid);
+
+    board.grid.forEach(
+      (row, rowNumber) => {
+        row.forEach(
+          (element, colNumber) => {
+
+            //create a square for each value of the array
+            let container = document.createElement("div");
+            container.className = "square";
+
+            //store its row and column as data
+            container.setAttribute("data-row", rowNumber);
+            container.setAttribute("data-col", colNumber);
+
+            gameGrid.append(container);
+          }
+        );
+      }
+    );
+  }
+
+  addGrid();
+
   resetButton.addEventListener("click", event => {
     board = new Board();
     hiddenGameOver.classList.add("hide");
     hiddenGameOver.classList.remove("show");
 
-    //reset all squares???
-    //change bg to white/transparent?
-    ///change innertext to nothing?
+    //delete the grid
+    gameGrid.remove();
+    addGrid();
+    gameGrid.addEventListener("click", gameGridClickListener);
+
   })
-
-  let gameGrid = document.createElement("div");
-  gameGrid.id = "board";
-  document.body.append(gameGrid);
-
-  board.grid.forEach(
-    (row, rowNumber) => {
-      row.forEach(
-        (element, colNumber) => {
-
-          //create a square for each value of the array
-          let container = document.createElement("div");
-          container.className = "square";
-          //container.innerText = `${rowNumber}, ${colNumber}`;
-
-          //store its row and column as data
-          container.setAttribute("data-row", rowNumber);
-          container.setAttribute("data-col", colNumber);
-
-          gameGrid.append(container);
-        }
-      );
-    }
-  );
 
   const gameGridClickListener = (event) =>  {
 
@@ -71,7 +78,6 @@ window.addEventListener("DOMContentLoaded", event => {
     if (board.isGameOver()) {
       hiddenGameOver.classList.add("show");
       hiddenGameOver.classList.remove("hide");
-
       gameGrid.removeEventListener("click", gameGridClickListener);
     }
   }
